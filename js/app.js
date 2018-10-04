925781fd762a9b72648b2f378d2c97dad0a4d285
Peter Schrock <peter@miskit.org>
2018-10-03 23:31:13 -0700

    fixed landing page, started working on about page

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$('.navbar li').click(function(e) {
+  //previous and current class names
+  const previousSelected = '.' + $('.selected').html().toLowerCase();
+  const currentSelected = '.' + $(e.target).html().toLowerCase();
+
+  //remove 'selected' class from old, add to current
+  $('.selected').removeClass('selected');
+  $(e.target).addClass('selected');
+
+  //remove previous and add current display
+  if (currentSelected == '.home') {
+    $('.overlay').show().animate({
+      opacity: 1,
+    }, 1000, () => {
+      $(previousSelected).hide();
+      $('.navbar').css('top','inherit').css('margin-top','1em').css('border-bottom','none');
+      $('.overlay').animate({
+        opacity: 0,
+      }, 2000, () => {
+        $('.overlay').hide();
+      });
+    });
+  } else if (previousSelected == '.home') {
+  $('.overlay').show().animate({
+    opacity: 1,
+  }, 1000, () => {
+    $(currentSelected).show();
+    $('.navbar').css('top','0').css('margin-top','0').css('border-bottom','5px solid rgba(0, 0, 0, 0.5)');
+    $('.overlay').animate({
+      opacity: 0,
+    }, 2000, () => {
+      $('.overlay').hide();
+    });
+  });
+  } else {
+    $('.overlay').show().animate({
+      opacity: 1,
+    }, 1000, () => {
+      $(previousSelected).hide();
+      $(currentSelected).show();
+      $('.overlay').animate({
+        opacity: 0,
+      }, 2000, () => {
+        $('.overlay').hide();
+      });
+    });
+  }
+});
