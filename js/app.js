e7e273c6c5f68e8becfbac0a3c88ea0c30eff2cd
Peter Schrock <peter@miskit.org>
2018-10-15 17:47:29 -0700

    created mobile first approach. change scroll magic for mobile device

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ const quotes = [
 //     $('cite').text(quotes[i].author);
 //   })
 // }
-
+let windowWidthCalc = $(window).width();
 
 const controller = new ScrollMagic.Controller();
 
@@ -58,7 +58,7 @@ const scene1 = new ScrollMagic.Scene({
 .setTween(shrinkNavbar)
 .addTo(controller);
 
-//ScrollMagic - About Heading Pinned
+//ScrollMagic - About Heading Bounce
 const bounceAboutImg = new TweenMax.from('.about .about-wrap-left', 1, {scale: 0.25, opacity: 0, ease:Bounce.easeOut});
 const scene2 = new ScrollMagic.Scene({
   triggerElement: '.about'
@@ -66,16 +66,22 @@ const scene2 = new ScrollMagic.Scene({
 .setTween(bounceAboutImg)
 .addTo(controller);
 
-const scene3 = new ScrollMagic.Scene({
-  triggerElement: ".about-wrap-left", // point of execution
-  duration: $('.about .about-wrap-right').height() - 400, // pin element for the 'main' height
-  triggerHook: 0.15, // don't trigger until #pinned-trigger1 hits the top of the viewport
-  reverse: true // allows the effect to trigger when scrolled in the reverse direction
-})
-.setPin('.about-wrap-left')
-.addTo(controller);
-
 //ScrollMagic - About Heading Pinned
+function aboutPinned(windowWidth) {
+  if (windowWidth >= 1024) {
+    const scene3 = new ScrollMagic.Scene({
+      triggerElement: ".about-wrap-left", // point of execution
+      duration: $('.about .about-wrap-right').height() - 400, // pin element for the 'main' height
+      triggerHook: 0.15, // don't trigger until #pinned-trigger1 hits the top of the viewport
+      reverse: true // allows the effect to trigger when scrolled in the reverse direction
+    })
+    .setPin('.about-wrap-left')
+    .addTo(controller);
+  }
+}
+aboutPinned(windowWidthCalc);
+
+//ScrollMagic - Portfolio Heading Bounce
 const bouncePrtfolioImg = new TweenMax.from('.portfolio .port-wrap-left', 1, {scale: 0.25, opacity: 0, ease:Bounce.easeOut});
 const scene4 = new ScrollMagic.Scene({
   triggerElement: '.portfolio'
@@ -84,14 +90,19 @@ const scene4 = new ScrollMagic.Scene({
 .addTo(controller);
 
 //ScrollMagic - Portfolio Heading Pinned
-const scene5 = new ScrollMagic.Scene({
-  triggerElement: ".port-wrap-left", // point of execution
-  duration: $('.portfolio .port-wrap-right').height() - 400, // pin element for the 'main' height
-  triggerHook: 0.15, // don't trigger until #pinned-trigger1 hits the top of the viewport
-  reverse: true // allows the effect to trigger when scrolled in the reverse direction
-})
-.setPin('.port-wrap-left')
-.addTo(controller);
+function portfolioPinned(windowWidth) {
+  if (windowWidth >= 1024) {
+    const scene5 = new ScrollMagic.Scene({
+      triggerElement: ".port-wrap-left", // point of execution
+      duration: $('.portfolio .port-wrap-right').height() - 400, // pin element for the 'main' height
+      triggerHook: 0.15, // don't trigger until #pinned-trigger1 hits the top of the viewport
+      reverse: true // allows the effect to trigger when scrolled in the reverse direction
+    })
+    .setPin('.port-wrap-left')
+    .addTo(controller);
+  }
+}
+portfolioPinned(windowWidthCalc);
 
 // ScrollMagic - Navigation to Pages
 $('.navbar li').click(function(e) {
